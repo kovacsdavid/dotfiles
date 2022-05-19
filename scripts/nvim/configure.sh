9c8f120a1c3ffce4ceb18392fb3fd27cd9d84097
Kovács Dávid <kapcsolat@kovacsdavid.com>
2022-05-19 15:31:23 +0200

    add zsh, nvim minor fixes

--- scripts/nvim/configure.sh
+++ scripts/nvim/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim && \
-mkdir -p ~/.config/nvim && \
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+mkdir -p ~/.config/nvim
 rsync -avhi ../../config/nvim/ ~/.config/nvim
---
+++ scripts/zsh/configure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat ../../config/zsh/zshrc > ~/.zshrc
