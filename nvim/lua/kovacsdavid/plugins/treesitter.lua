767b2ef95bc4021d6e701da837ee8dc2b5f86bb5
Kovács Dávid <kapcsolat@kovacsdavid.dev>
2024-08-18 19:11:50 +0200

    Add treesitter plugin

---
+++ nvim/lua/kovacsdavid/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = {
+              "lua",
+              "vim",
+              "vimdoc",
+              "query",
+              "javascript",
+              "html",
+              "go",
+              "goctl",
+              "json",
+              "php",
+              "sql",
+          },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
