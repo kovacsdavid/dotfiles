0f2463b881d249c78729643a1ec03a472fb07d5b
Kovács Dávid <kapcsolat@kovacsdavid.dev>
2024-08-28 20:36:50 +0200

    Add mason plugin and update volar and tsserver cfg

--- nvim/lua/kovacsdavid/plugins/lsp-zero.lua
+++ nvim/lua/kovacsdavid/plugins/lsp-zero.lua
@@ -1,77 +1,85 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v4.x',
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-  },
-  config = function()
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v4.x',
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
         -- lsp-zero
-    local lsp_zero = require('lsp-zero')
+        local lsp_zero = require('lsp-zero')
 
-    -- lsp_attach is where you enable features that only work
-    -- if there is a language server active in the file
-    local lsp_attach = function(client, bufnr)
-        local opts = {buffer = bufnr}
+        -- lsp_attach is where you enable features that only work
+        -- if there is a language server active in the file
+        local lsp_attach = function(client, bufnr)
+            local opts = {buffer = bufnr}
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+            vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+            vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+            vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+            vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+            vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+            vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+            vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+            vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+            vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end
+
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        lsp_zero.extend_lspconfig({
+            sign_text = true,
+            lsp_attach = lsp_attach,
+            capabilities = capabilities,
+        })
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            -- Replace the language servers listed here
+            -- with the ones you want to install
+            ensure_installed = {
+                'tsserver',
+                'phpactor',
+                'eslint',
+                'volar',
+                'lua_ls',
+            },
+            handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+                ["volar"] = function()
+                    require("lspconfig").volar.setup({
+                        -- NOTE: https://github.com/vuejs/language-tools/issues/4706
+                        capabilities = capabilities,
+                        init_options = {
+                            vue = {
+                                hybridMode = false,
+                            },
+                        },
+                    })
+                end,
+                ["tsserver"] = function()
+                    local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
+                    local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
+
+                    require("lspconfig").tsserver.setup({
+                        capabilities = capabilities,
+                        init_options = {
+                            plugins = {
+                                {
+                                    name = "@vue/typescript-plugin",
+                                    location = volar_path,
+                                    languages = { "vue" },
+                                },
+                            },
+                        },
+                    })
+                end,
+            },
+        })
     end
-
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    lsp_zero.extend_lspconfig({
-        sign_text = true,
-        lsp_attach = lsp_attach,
-        capabilities = capabilities,
-    })
-
-    local home = os.getenv("HOME")
-
-    require('lspconfig').tsserver.setup({
-        capabilities = capabilities,
-      init_options = {
-        plugins = {
-          {
-            name = "@vue/typescript-plugin",
-            location = home .. "/.nvm/versions/node/v22.6.0/lib/node_modules/@vue/typescript-plugin",
-            languages = {"javascript", "typescript", "vue"},
-          },
-        },
-      },
-      filetypes = {
-        "javascript",
-        "typescript",
-        "vue",
-      },
-    })
-    require('lspconfig').phpactor.setup({
-        capabilities = capabilities,
-    })
-    require('lspconfig').volar.setup({
-        capabilities = capabilities,
-        filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-        init_options = {
-            vue = {
-                -- hybridMode = false,
-                hybridMode = true,
-            },
-            typescript = {
-                tsdk = home .. "/.nvm/versions/node/v22.6.0/lib/node_modules/typescript/lib",
-            },
-        },
-    })
-    require('lspconfig').eslint.setup({
-        capabilities = capabilities,
-    })
-
-  end
 }
 
