a42106687c31d7e99db353bd039981c5e552e6a0
Kovács Dávid <kapcsolat@kovacsdavid.dev>
2024-09-04 07:59:58 +0200

    Add lua_ls config

--- nvim/lua/kovacsdavid/plugins/lsp-zero.lua
+++ nvim/lua/kovacsdavid/plugins/lsp-zero.lua
@@ -50,6 +50,29 @@ return {
                 function(server_name)
                     require('lspconfig')[server_name].setup({})
                 end,
+                ["lua_ls"] = function ()
+                    require('lspconfig').lua_ls.setup({
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    globals = {
+                                        'vim',
+                                        'require'
+                                    },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                },
+                                telemetry = {
+                                    enable = false,
+                                },
+                            },
+                        },
+                    })
+                end,
                 ["volar"] = function()
                     require("lspconfig").volar.setup({
                         -- NOTE: https://github.com/vuejs/language-tools/issues/4706
