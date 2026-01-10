c677c72a18bc8971b88aba71113e7a81491448f2
Kovács Dávid <kapcsolat@kovacsdavid.dev>
2026-01-10 16:23:32 +0100

    [ADD] nvim disable copilot
    Signed-off-by: Kovács Dávid <kapcsolat@kovacsdavid.dev>

--- nvim/init.lua
+++ nvim/init.lua
@@ -240,26 +240,26 @@ require("lazy").setup({
         },
       },
     },
-    {
-      "github/copilot.vim"
-    },
-    {
-      "CopilotC-Nvim/CopilotChat.nvim",
-      dependencies = {
-        { "nvim-lua/plenary.nvim", branch = "master" },
-      },
-      opts = {
-        {
-          model = 'gpt-4.1',           -- AI model to use
-          temperature = 0.1,           -- Lower = focused, higher = creative
-          window = {
-            layout = 'vertical',       -- 'vertical', 'horizontal', 'float'
-            width = 0.5,              -- 50% of screen width
-          },
-          auto_insert_mode = true,     -- Enter insert mode when opening
-        }
-      },
-    },
+--    {
+--      "github/copilot.vim"
+--    },
+--    {
+--      "CopilotC-Nvim/CopilotChat.nvim",
+--      dependencies = {
+--        { "nvim-lua/plenary.nvim", branch = "master" },
+--      },
+--      opts = {
+--        {
+--          model = 'gpt-4.1',           -- AI model to use
+--          temperature = 0.1,           -- Lower = focused, higher = creative
+--          window = {
+--            layout = 'vertical',       -- 'vertical', 'horizontal', 'float'
+--            width = 0.5,              -- 50% of screen width
+--          },
+--          auto_insert_mode = true,     -- Enter insert mode when opening
+--        }
+--      },
+--    },
     {
       "mbbill/undotree"
     },
